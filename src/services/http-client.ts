41ce18159be2c70bfeaa54c358ac66412784ce4d
houstondapaz <contact@houstondapaz.dev>
2024-12-20 13:01:48 -0300

    fix: VITE API URL

--- src/services/http-client.ts
+++ src/services/http-client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseURL = 'http://localhost:3000'
+export const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
 
 export const httpClient = axios.create({
     baseURL,
